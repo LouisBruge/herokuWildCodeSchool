017f0319ba1cbd36dff410ec4afc174ccf3b63f3
louis bruge <louis.bruge@gmail.com>
2021-02-13 11:24:21 +0100

    feat: migrations, seeds and build web app at build step

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 
 const { Item } = require("./dataLayer/models");
 
 const PORT = process.env.PORT ?? 8080;
+const WEB_APP_BUILD_PATH = "web-app/build";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({mesage: "Hello World!"});
-});
+app.use(express.static(path.join(__dirname, WEB_APP_BUILD_PATH)));
 
 app.get("/api/items", (req, res) => {
   Item.findAll()
     .then(items => res.send({items}))
     .catch(e => res.send(e.message || e));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, WEB_APP_BUILD_PATH, "index.html"));
 });
 
 app.listen(PORT, () => {
